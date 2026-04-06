f8350e657ed90b28af978a4b75daaf017f698071
nguyenlehoang358 <hoangngng36@gmail.com>
2026-04-07 00:01:04 +0700

    fix: add missing PreAuthorize import to restore build stability

--- src/main/java/com/myweb/controller/ExternalSecurityController.java
+++ src/main/java/com/myweb/controller/ExternalSecurityController.java
@@ -6,6 +6,7 @@ import com.myweb.repository.ExternalClientSiteRepository;
 import com.myweb.repository.ExternalSecurityAlertRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
